7cfaaadd0f182f63f855205e33dc50d0b3384fb1
Beeno Tung <aabbcc1241@yahoo.com.hk>
2016-01-19 17:15:32 +0800

    init commit

---
+++ src/main/scala/com/example/Main.scala
@@ -0,0 +1,19 @@
+package com.example
+
+import akka.actor._
+
+object Main extends App {
+
+  val nodeConfig = NodeConfig parse args
+
+  // If a config could be parsed - start the system
+  nodeConfig map { c =>
+    val system = ActorSystem(c.clusterName, c.config)
+
+    // Register a monitor actor for demo purposes
+    system.actorOf(Props[MonitorActor], "cluster-monitor")
+
+    system.log info s"ActorSystem ${system.name} started successfully"
+  }
+
+}
