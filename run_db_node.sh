d8e11a931d7130fd2fc672514adbddd81ed8f76d
Beeno Tung <aabbcc1241@yahoo.com.hk>
2016-04-09 00:58:10 +0800

    updated ip to polyu vm

--- run_db_node.sh
+++ run_db_node.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 rm -rf ./rethinkdb_data
-#rethinkdb --join 58.96.176.223:29015 --bind all
-rethinkdb --join 128.199.97.91:29015 --bind all
+
+# aliyun
+# rethinkdb --join 58.96.176.223:29015 --bind all
+
+# janus
+# rethinkdb --join 128.199.97.91:29015 --bind all
+
+# polyu
+rethinkdb --join 158.132.8.147:29015 --bind all
