8e5ff89ab843ac6b47286d1d9fa17a6f4aa458fe
Beeno Tung <aabbcc1241@yahoo.com.hk>
2016-04-08 22:56:26 +0800

    changed database ip

--- run_db_node.sh
+++ run_db_node.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 rm -rf ./rethinkdb_data
-rethinkdb --join 58.96.176.223:29015 --bind all
+#rethinkdb --join 58.96.176.223:29015 --bind all
+rethinkdb --join 128.199.97.91:29015 --bind all
