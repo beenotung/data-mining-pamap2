fd43bf4da078b55d40f7652ceb8e1eea3fa6c8af
Beeno Tung <aabbcc1241@yahoo.com.hk>
2016-04-13 04:57:28 +0800

    fixed typo

--- src/script/remoteRun.sh
+++ src/script/remoteRun.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-grep -v '#' ips.txt | sed 's/ /\n/g' | xargs -I {} echo sshpass -p node ssh -oStrictHostKeyChecking=no node@{} "'cd data-mining-pamap2; opt/sbt runCompute.sh' &" | source /dev/stdin
+grep -v '#' ips.txt | sed 's/ /\n/g' | xargs -I {} echo sshpass -p node ssh -oStrictHostKeyChecking=no node@{} "'cd data-mining-pamap2; opt/sbt runCompute' &" | source /dev/stdin
