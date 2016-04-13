2a213f9dcc9bade5da2ad262d53df448cb70c637
Beeno Tung <aabbcc1241@yahoo.com.hk>
2016-04-13 15:10:33 +0800

    added temp script

---
+++ src/script/remoteTemp.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+grep -v '#' ips.txt | sed 's/ /\n/g' | xargs -I {} echo "echo sending to {};" sshpass -p node ssh -oStrictHostKeyChecking=no node@{} "'echo sent to {}; cd data-mining-pamap2; rm .git/objects -rf; git init;./update.sh' &" | source /dev/stdin
